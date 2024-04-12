164a9fc5a44f4b4629ac8282b8042880a1eebd73
Diego <dbwcontrol05@gmail.com>
2024-04-12 07:28:46 -0600

    Ejercio de arreglo num pares

---
+++ numerosPares.c
@@ -0,0 +1,19 @@
+/*Almacenar 5 numeros pares.*/
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int numbers[5];
+    numbers[0]= 2;
+    numbers[1]=44;
+    numbers[2]=106;
+    numbers[3]=1000;
+    numbers[4]=2024;
+
+    printf("Elemento # 1: %d\n", numbers[0]);
+    printf("Elemento # 2: %d\n", numbers[1]);
+    printf("Elemento # 3: %d\n", numbers[2]);
+    printf("Elemento # 4: %d\n", numbers[3]);
+    printf("Elemento # 5: %d\n", numbers[4]);
+    return 0;
+}

886c54fb7b92ba16a9d691564dcf066b9e26761b
jadg13 <jose.duran@uamv.edu.ni>
2024-06-11 11:34:16 -0600

    Sesión 11/6/24 continuara con editar

--- sesion070624/daoCiudad.cpp
+++ sesion070624/daoCiudad.cpp
@@ -6,9 +6,9 @@
 using namespace std;
 
 CIUDAD ciudades[MAX_REG];
-int pos=0;
+int pos = 0;
 
-//crud
+// crud
 int obtPos(int id);
 void agregar(CIUDAD *c);
 void editar(CIUDAD *c, int id);
@@ -18,46 +18,59 @@ int menu();
 void principal();
 void pedirDatos();
 void mostrarDatos();
+void buscarxID();
+void editarDatos();
 
-void agregar(CIUDAD *c){
+void agregar(CIUDAD *c)
+{
     ciudades[pos] = *c;
     pos++;
 }
 
-CIUDAD buscar(int id){
-    for(int i = 0; i<pos; i++){
-        if(id == ciudades[i].id){
+CIUDAD buscar(int id)
+{
+    for (int i = 0; i < pos; i++)
+    {
+        if (id == ciudades[i].id)
+        {
             return ciudades[i];
         }
     }
     CIUDAD c;
     return c;
-} 
+}
 
-int obtPos(int id){
-    for(int i = 0; i < pos; i++){
-        if(ciudades[i].id == id){
+int obtPos(int id)
+{
+    for (int i = 0; i < pos; i++)
+    {
+        if (ciudades[i].id == id)
+        {
             return i;
         }
     }
     return -1;
 }
 
-void editar(CIUDAD *c, int id){
+void editar(CIUDAD *c, int id)
+{
     int posi = obtPos(id);
-    strcpy(ciudades[posi].nombre, c->nombre); 
+    strcpy(ciudades[posi].nombre, c->nombre);
     strcpy(ciudades[posi].descripcion, c->descripcion);
 }
 
-void eliminar(int id){
+void eliminar(int id)
+{
     int posi = obtPos(id);
-    for(int i = posi; i<pos-1; i++){
-        ciudades[i] = ciudades[i+1];
+    for (int i = posi; i < pos - 1; i++)
+    {
+        ciudades[i] = ciudades[i + 1];
     }
     pos--;
 }
 
-int menu(){
+int menu()
+{
     setlocale(LC_ALL, "spanish");
     int op;
     cout << "Menu \n";
@@ -72,29 +85,36 @@ int menu(){
     return op;
 }
 
-void principal(){
+void principal()
+{
     int op;
-    do{
+    do
+    {
         op = menu();
-        switch(op){
-            case 1:
-                pedirDatos();
-                break;
-            case 5:
-                mostrarDatos();
-                break;
-            case 6:
-                cout << "Adios, mi tierno\n";
-                break;
-            default:
-                cout << "No seas neofito si solo hay 6\n";
-                break;
+        switch (op)
+        {
+        case 1:
+            pedirDatos();
+            break;
+        case 4:
+            buscarxID();
+            break;
+        case 5:
+            mostrarDatos();
+            break;
+        case 6:
+            cout << "Adios, mi tierno\n";
+            break;
+        default:
+            cout << "No seas neofito si solo hay 6\n";
+            break;
         }
 
-    }while(op !=6);
+    } while (op != 6);
 }
 
-void pedirDatos(){
+void pedirDatos()
+{
     CIUDAD ciudad;
     cout << "Datos de Ciudad\n";
     cout << "ID: ";
@@ -107,12 +127,30 @@ void pedirDatos(){
     cout << "Registro Agregado....\n";
 }
 
-void mostrarDatos(){
-    for(int i = 0; i < pos; i++){
+void mostrarDatos()
+{
+    for (int i = 0; i < pos; i++)
+    {
         cout << "ID: " << ciudades[i].id << endl;
         cout << "Nombre: " << ciudades[i].nombre << endl;
         cout << "Descripcion " << ciudades[i].descripcion << endl;
     }
 }
 
+void buscarxID()
+{
+    int id;
+    cout << "Dime el ID de la ciudad a buscar: ";
+    cin >> id;
+    CIUDAD c;
+    c = buscar(id);
+    cout << "==============================" << endl;
+    cout << c.id << endl;
+    cout << c.nombre << endl;
+    cout << c.descripcion << endl;
+    cout << "==============================" << endl;
+}
 
+void editarDatos(){
+    int id
+}
--- sesion070624/principal.cpp
+++ sesion070624/principal.cpp
@@ -8,6 +8,7 @@ int main(int argc, char const *argv[])
 {
     /* code */
     setlocale(LC_ALL, "spanish");
+   
     principal();
     return 0;
 }
